8035068d320419824f842d73e04fb8a4e4cba55e
Muhammad bilal <meharbilal24525@gmail.com>
2023-12-03 13:16:49 +0500

    Add project files.

---
+++ Constructor and Constructor Overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Constructor_and_Constructor_Overloading
+{
+    class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("Constructor--Default Constructor");
+        }
+
+        public Program(int a, int b)
+        {
+            Console.WriteLine("This is Int Constructor: {0}",(a+b));
+        }
+
+        public Program(float a, float b=12.2f)
+        {
+            Console.WriteLine("This is 3rd constructor:{0}",(a+b));
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Program FirstConstructor = new Program(10, 20);
+            Program SecondConstructor = new Program(10.23f, 1212.1f);
+            Program thirdConstructor = new Program(10.23f);
+            Console.ReadLine();
+        }
+    }
+}
